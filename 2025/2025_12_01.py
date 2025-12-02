07f0b731441012c17b007d7f3b649e1cbad017db
Katka Halasova <katarina.kaderova@gmail.com>
2025-12-02 22:55:51 +0100

    part two of day 1 2025

--- 2025/2025_12_01.py
+++ 2025/2025_12_01.py
@@ -1,5 +1,3 @@
-import math
-
 #  Part 1
 # example
 f = ['L68',
@@ -32,38 +30,42 @@ def rotation_to_plus_minus(rotation, distance):
     else:
         return KeyError
     
-def dail_ends_at_zero(rotations, distances, starting_point):
-    modulo_point = starting_point
+def dial_ends_at_zero(rotations, distances, starting_point):
     zero_counter = 0 
     for i in range(len(rot)):
-        point = modulo_point + rotation_to_plus_minus(rot[i],dist[i])
+        point = starting_point + rotation_to_plus_minus(rot[i],dist[i])
         # dialing is in range 0-100, lets use modulo to keep range
-        modulo_point = point % 100
-        if modulo_point == 0:
+        starting_point = point % 100
+        if starting_point == 0:
             zero_counter += 1
     return zero_counter
     
-print (f'Number of dials ending at zero: {dail_ends_at_zero(rot, dist, 50)}')
+print (f'Number of dials ending at zero: {dial_ends_at_zero(rot, dist, 50)}')
 
 
 # Part 2
 
 # any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one
 
-modulo_point = 50
+starting_point = 50
 clicks_at_zero = 0 
 for i in range(len(rot)):
-    point = modulo_point + rotation_to_plus_minus(rot[i],dist[i])
-    if point == 0:
-        clicks_at_zero += 1
-    elif point % 100 == 0:
-        clicks_at_zero = math.trunc(dist[i]/100) + 1 
-    elif point < 0 or point > 100:
-        clicks_at_zero += round(modulo_point + dist[i] / 100) 
-    modulo_point = point % 100
-
-    
+    # number of cycles (dialing is more then one cycle aka 100 clicks)
+    cycles = dist[i] // 100
+    # what left after cycles
+    remainder = dist[i] % 100 
+    # special case when starting point is zero
+    if starting_point == 0:
+        clicks_at_zero += cycles
+    else:
+        # number of clicks at zero = number of cycles + 0 (because (starting_point + remainder) is less then one cycle)
+        if (rotation_to_plus_minus(rot[i],dist[i]) < 0 and starting_point - remainder > 0) or (rotation_to_plus_minus(rot[i],dist[i]) > 0 and starting_point + remainder < 100):
+            clicks_at_zero += cycles
+        # number of clicks at zero = number of cycles + 1 (because (starting_point + remainder) is more then one cycle) 
+        elif (rotation_to_plus_minus(rot[i],dist[i]) < 0 and starting_point - remainder <= 0) or (rotation_to_plus_minus(rot[i],dist[i]) > 0 and starting_point + remainder >= 100):
+            clicks_at_zero += cycles + 1
+    # update starting point
+    point = starting_point + rotation_to_plus_minus(rot[i],dist[i])
+    starting_point = point % 100
         
-print(clicks_at_zero)
-# 6775 too low
-# 10396 too high
+print (f'Number of dials ending at zero: {clicks_at_zero}')
